e267a512aa967358f86f9500e81351507e11f22e
mrrased <rashedtechdr.786@gmail.com>
2023-03-13 18:27:11 +0600

    added three component

--- src/Component/Home/AboutUs/AboutUs.tsx
+++ src/Component/Home/AboutUs/AboutUs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '@mui/material/Container';
+import  'tw-elements';
 
 
 const AboutUs = () => {
@@ -11,7 +12,26 @@ const AboutUs = () => {
                     <h2 className='font-normal text-[#36187d] Bellota_Text text-[2.8rem]'>Secure Features</h2>
                     <p className='font-normal text-[#6c758f] Bellota_Text text-[1.2rem] text-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elitdo eiusmod temporeum dicant partem scripserit.</p>
                 </div>
-                <div>02</div>
+                <div>
+                    <h5 className='font-normal text-[#6c758f] Bellota_Text text-[1.2rem] text-left '>Home Security</h5>
+                    <div className="w-full bg-neutral-200 dark:bg-neutral-600 mb-10 rounded-md">
+                        <div className="bg-[#36187d] rounded-l-md p-0.5 text-center text-xs font-medium leading-none text-white w-[92%] ">
+                            92%
+                        </div>
+                    </div>
+                    <h5 className='font-normal text-[#6c758f] Bellota_Text text-[1.2rem] text-left '>Office Security</h5>
+                    <div className="w-full bg-neutral-200 dark:bg-neutral-600 mb-10 rounded-md">
+                        <div className="bg-[#36187d] rounded-l-md p-0.5 text-center text-xs font-medium leading-none text-white w-[71%] ">
+                            71%
+                        </div>
+                    </div>
+                    <h5 className='font-normal text-[#6c758f] Bellota_Text text-[1.2rem] text-left '>CCTV Cameras</h5>
+                    <div className="w-full bg-neutral-200 dark:bg-neutral-600 rounded-md">
+                        <div className="bg-[#36187d] rounded-l-md p-0.5 text-center text-xs font-medium leading-none text-white w-[53%]">
+                            53%
+                        </div>
+                    </div>
+                </div>
             </div>
         </Container>
     );
